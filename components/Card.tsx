6fbfc7bef19c894b615724f1e8c48998f6b0c470
branmkim <bran.mkim@gmail.com>
2023-09-15 23:33:38 -0400

    updated frc jpeg

--- components/Card.tsx
+++ components/Card.tsx
@@ -7,8 +7,8 @@ type CardProps = {
 }
 function Card({ img, title, subtitle } : CardProps) {
     return (
-        <div className="card-bg flex flex-col flex-none w-4/5 sm:w-80 min-h-fit sm:h-60 overflow-clip mr-4 rounded-lg">
-            <img src={img} className="h-3/5 object-cover" />
+        <div className="card-bg flex flex-col flex-none w-4/5 sm:w-80 min-h-fit sm:h-64 overflow-clip mr-4 rounded-lg">
+            <img src={img} className="h-2/3 object-cover" />
             <div className="w-full h-full p-4">
                 <p><b>{ title }</b></p>
                 <p className="text-xs">{ subtitle }</p>
